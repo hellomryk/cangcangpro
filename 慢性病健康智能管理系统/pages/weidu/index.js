a9588e9c1b1b618788206a01141140a7abcc8845
wangzhenhuagit <wangzhenhua_211314@163.com>
2018-10-23 10:52:28 +0800

    w1023

--- 慢性病健康智能管理系统/pages/weidu/index.js
+++ 慢性病健康智能管理系统/pages/weidu/index.js
@@ -68,26 +68,26 @@ Page({
       //保存
       var bean = JSON.parse(options.za);
       var bean1 = JSON.parse(options.zheng);
-      bean1=bean1.join("、")
+      bean1=bean1.join(" ")
       console.log(bean1)
       wx.request({
-          url: 'https://chronic.infobigdata.com/doctorapplet/f52024d75d4348f38cdad3670d209c1e/doctorqs',
+          url: 'http://192.168.1.56:8080/newrobot5',
           data: {
-              issue: encodeURI("保存问诊"),
-              openid: encodeURI(bean.openId),
-              clazzstep: encodeURI("2"),
-              baseinfo: encodeURI(bean.username + " " + bean.sex + " " + bean.age ),
-              questioningsymptoms: encodeURI(bean1),
-            diseaseprobability:encodeURI("85%"),
-                //   evaluation:encodeURI("严重"),
-              diseasename: encodeURI(options.id),
-                //   trend:encodeURI("↑")
+
+              name: bean.username,
+              sex: bean.sex,
+              old: bean.age, 
+              diseaseName: options.id,
+              symptom: bean1, //（多个症状用空格分开）
+              openid: bean.openId,
+              inLocation: '北京市', //（所在地）
+              date: ' ' //（实时时间，精确到秒，年月日时分秒）
 
                  },
           header: {
-              'content-type': 'application/json' // 默认值
+              'content-type': 'application/json' // 默认值application/x-www-form-urlencoded
           },
-        //   method: "POST",
+           method: "POST",
           success: function (res) {
               console.log(res.data)
               _this.setData({
