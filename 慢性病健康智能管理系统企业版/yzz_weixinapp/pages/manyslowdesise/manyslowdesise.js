46c9f658247aa890f89ff3923e1f6ac4a5744884
hellomryk <3043156522@qq.com>
2018-10-29 16:07:25 +0800

    2018-10-29 4:07

--- 慢性病健康智能管理系统企业版/yzz_weixinapp/pages/manyslowdesise/manyslowdesise.js
+++ 慢性病健康智能管理系统企业版/yzz_weixinapp/pages/manyslowdesise/manyslowdesise.js
@@ -1,23 +1,28 @@
 // pages/manyslowdesise/manyslowdesise.js
 var app = getApp();
+var _this = null;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    foodlistarr:[]
+    foodlistarr:[],
+    flsb_inputval:''//搜索框输入内容
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    _this = this;
     const selfPage = this;
+    const keyword=null;
     wx.request({
         url: 'https://chronic.infobigdata.com/doctorapplet/f52024d75d4348f38cdad3670d209c1e/wiki',
       data: {
         wikiid: 0,//0代表查列表，具体某一个id表示查具体某一条
+        keyword: encodeURI('支气管炎')
       },
       method: 'get',
       success: function (res) {
@@ -29,7 +34,32 @@ Page({
       }
     })
   },
-
+  // 搜索框输入内容
+  flsb_input(e) {
+      const value = e.detail.value;
+      _this.setData({
+        flsb_inputval:value
+      })
+  },
+  // 输入框确认事件
+  flsb_confirm() {
+    wx.request({
+      url:'https://chronic.infobigdata.com/doctorapplet/f52024d75d4348f38cdad3670d209c1e/wiki',
+      data: {
+        wikiid: 0,//0代表查列表，具体某一个id表示查具体某一条
+        keyword: encodeURI(_this.data.flsb_inputval)
+      },
+      method:"GET",
+      header: {
+        "Content-Type":"application/json"
+      },
+      success(res) {
+        selfPage.setData({
+          foodlistarr: JSON.parse(res.data.data)
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
